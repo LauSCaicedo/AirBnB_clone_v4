6edd764285709d4849d0e83a02ff17b7c2cdbf7c
LauSCaicedo <3383@holbertonschool.com>
2022-02-11 00:13:29 -0500

    Update Files

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-( document ).ready(function input_check() {
+$( document ).ready(function input_check() {
     let list_id = [];
     let check = document.querySelector('input[type="checkbox"]');
     let Text = document.querySelector('input[data-id=":amenity_id"]');
